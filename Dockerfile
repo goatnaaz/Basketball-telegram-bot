ff291bfe0ae810137ef1bea300fdc1913204b25f
Nazar Kozlovsky <nazar.kozlovsky@gmail.com>
2022-07-01 17:17:11 +0300

    STEP_6_BTB - added database and repository layer

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM adoptopenjdk/openjdk11:ubi
-ARG JAR_FILE=target/*.jar
 ENV BOT_NAME=test_btb_bot
-ENV BOT_TOKEN=5342919530:AAGJvKvdw_h1fiSKQh5jLbR_iVR8n2pvtdg
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
+ENV BOT_TOKEN=5424983064:AAH3QNgDT1GhG2lHuqN4I8whDjXDoj0Wpe8
+ENV BOT_DB_USERNAME=BTB_db
+ENV BOT_DB_PASSWORD=7410
+
+ENTRYPOINT ["java","-Dspring.datasource.password=${BOT_DB_PASSWORD}", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-Dspring.datasource.username=${BOT_DB_USERNAME}", "-jar", "app.jar"]
--- src/main/java/com/example/Basketballtelegrambot/command/StopCommand.java
+++ src/main/java/com/example/Basketballtelegrambot/command/StopCommand.java
@@ -1,6 +1,7 @@
 package com.example.Basketballtelegrambot.command;
 
 import com.example.Basketballtelegrambot.service.SendBotMessageService;
+import com.example.Basketballtelegrambot.service.TelegramUserService;
 import org.telegram.telegrambots.meta.api.objects.Update;
 /**
  * Stop {@link Command}.
@@ -9,16 +10,23 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class StopCommand implements  Command{
 
     private final SendBotMessageService sendBotMessageService;
+    private final TelegramUserService telegramUserService;
 
     public static final String STOP_MESSAGE = "Deactivated everything \uD83D\uDE1F.";
 
 
-    public StopCommand(SendBotMessageService sendBotMessageService) {
+    public StopCommand(SendBotMessageService sendBotMessageService , TelegramUserService telegramUserService) {
         this.sendBotMessageService = sendBotMessageService;
+        this.telegramUserService = telegramUserService;
     }
 
     @Override
     public void execute(Update update) {
         sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), STOP_MESSAGE);
+        telegramUserService.findByChatId(update.getMessage().getChatId().toString())
+                .ifPresent(it -> {
+                    it.setActive(false);
+                    telegramUserService.save(it);
+                });
     }
 }
